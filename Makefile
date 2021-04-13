7e1bbacfaaaed85c3d870b306bd8f9c518b4070d
nonk123 <Sergeyii321@gmail.com>
2021-04-13 15:22:09 +0300

    Finish the first labwork in the new semester

---
+++ Makefile
@@ -0,0 +1,5 @@
+CPPFLAGS = /EHsc
+
+all: L1
+
+L1: Z1_1.exe Z1_2.exe Z1_3.exe Z1_4.exe
---
+++ common.hpp
@@ -0,0 +1,32 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <conio.h>
+#include <math.h>
+#include <time.h>
+
+#include <windows.h>
+
+/* random() and randomize() are not defined on university computers. */
+
+#ifndef randomize
+#define randomize() (srand (time (NULL)))
+#endif /* randomize */
+
+#ifndef random
+#define random(X) (rand () % (X))
+#endif /* random */
+
+inline void
+fix_locale ()
+{
+  SetConsoleCP (1251);
+  SetConsoleOutputCP (1251);
+}
+
+using namespace std;
+
+#endif /* COMMON_HPP */
